857952c5bb53f7a3014d5c6d807ab32d1501e1ef
Ruan Scherer <ruan.vscherer@gmail.com>
2020-08-05 19:18:46 -0300

    :sparkles: Get total connections from API

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import logoImg from '../../assets/images/logo.svg'
 import landingImg from '../../assets/images/landing.svg'
 import studyIcon from '../../assets/images/icons/study.svg'
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
+import api from '../../services/api'
 import './styles.css'
 
 function Landing() {
+    const [totalConnections, setTotalConnections] = useState(0)
+
+    useEffect(() => {
+        api.get('/connections')
+            .then(response => setTotalConnections(response.data.total))
+            .catch(err => console.log(err))
+    })
+
     return (
         <div id="page-landing">
             <div id="page-landing-content" className="container">
@@ -34,7 +43,7 @@ function Landing() {
                 </div>
                 
                 <span className="total-connections">
-                    Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
+                    Total de {totalConnections} conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
                 </span>
             </div>
         </div>
